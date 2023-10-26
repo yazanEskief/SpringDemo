cd8b06f62ca4b603deeabf3a99cac7424801ae2f
Yazan Eskief <yazaniskef@gmail.com>
2023-10-26 15:20:56 +0200

    initial commit

---
+++ src/main/java/de/fhws/fiw/fds/springDemoApp/SpringDemoAppApplication.java
@@ -0,0 +1,13 @@
+package de.fhws.fiw.fds.springDemoApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDemoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDemoAppApplication.class, args);
+	}
+
+}
