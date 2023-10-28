9380821e5d28e9608ef9c1d14dcbaaa33c44fa9d
Yazan Eskief <yazaniskef@gmail.com>
2023-10-28 23:09:03 +0200

    create the entities

---
+++ src/main/java/de/fhws/fiw/fds/springDemoApp/entity/Location.java
@@ -0,0 +1,101 @@
+package de.fhws.fiw.fds.springDemoApp.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "location")
+public class Location {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "city_name")
+    private String cityName;
+
+    @Column(name = "longitude")
+    private double longitude;
+
+    @Column(name = "latitude")
+    private double latitude;
+
+    @Column(name = "visitedOn")
+    private LocalDate visitedOn;
+
+    @ManyToOne()
+    @JoinColumn(name = "person_id")
+    @JsonIgnore
+    private Person person;
+
+    public Location() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public LocalDate getVisitedOn() {
+        return visitedOn;
+    }
+
+    public void setVisitedOn(LocalDate visitedOn) {
+        this.visitedOn = visitedOn;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+    }
+
+    public void updateLocation(final Location newLocation) {
+        this.cityName = newLocation.cityName;
+        this.latitude = newLocation.latitude;
+        this.longitude = newLocation.longitude;
+        this.visitedOn = newLocation.visitedOn;
+    }
+
+    @Override
+    public String toString() {
+        return "Location{" +
+                "id=" + id +
+                ", cityName='" + cityName + '\'' +
+                ", longitude=" + longitude +
+                ", latitude=" + latitude +
+                ", visitedOn=" + visitedOn +
+                '}';
+    }
+}
---
+++ src/main/java/de/fhws/fiw/fds/springDemoApp/entity/Person.java
@@ -0,0 +1,126 @@
+package de.fhws.fiw.fds.springDemoApp.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Entity
+@Table(name = "person")
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "birth_date")
+    private LocalDate birthDate;
+
+    @Column(name = "email_address")
+    private String emailAddress;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "person")
+    @JsonIgnore
+    private List<Location> locations;
+
+    public Person() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthdate) {
+        this.birthDate = birthdate;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public List<Location> getLocations() {
+        return locations;
+    }
+
+    public void setLocations(List<Location> locations) {
+        this.locations = locations;
+    }
+
+    public void addLocation(Location location) {
+        if(locations == null) {
+            locations = new ArrayList<>();
+        }
+
+        locations.add(location);
+    }
+
+    public void addAllLocations(Location... locations) {
+        Arrays.asList(locations).forEach(this::addLocation);
+    }
+
+    public void addAllLocations(final List<Location> locations) {
+        if(this.locations == null) {
+            this.locations = new ArrayList<>();
+        }
+        this.locations.addAll(locations);
+    }
+
+    public void updatePerson(Person newPerson) {
+        this.firstName = newPerson.firstName;
+        this.lastName = newPerson.lastName;
+        this.birthDate = newPerson.birthDate;
+        this.emailAddress = newPerson.emailAddress;
+    }
+
+    public void removeLocation(Location locationToRemove) {
+        locations.removeIf(location -> location.getId() == locationToRemove.getId());
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", birthdate=" + birthDate +
+                ", emailAddress='" + emailAddress + '\'' +
+                '}';
+    }
+}
