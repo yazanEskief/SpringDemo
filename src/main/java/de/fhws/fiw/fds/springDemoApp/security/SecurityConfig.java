f6a0e1bf1a4fcbd07aeef7d89d8080b97c6ddcb0
Yazan Eskief <yazaniskef@gmail.com>
2023-10-28 23:14:18 +0200

    create a simple security config class to disable the authorization temporarily

---
+++ src/main/java/de/fhws/fiw/fds/springDemoApp/security/SecurityConfig.java
@@ -0,0 +1,30 @@
+package de.fhws.fiw.fds.springDemoApp.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests(configurer -> configurer
+                .requestMatchers(HttpMethod.GET, "api/**").permitAll()
+                .requestMatchers(HttpMethod.POST, "api/**").permitAll()
+                .requestMatchers(HttpMethod.PUT, "api/**").permitAll()
+                .requestMatchers(HttpMethod.DELETE, "api/**").permitAll()
+        );
+
+        http.httpBasic(Customizer.withDefaults());
+
+        http.csrf(AbstractHttpConfigurer::disable);
+
+        return http.build();
+    }
+}
